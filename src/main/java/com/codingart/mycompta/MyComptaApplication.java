f68dcce26c300655597cbd483e7b7b377077609b
MarwaneBella <bellaworks00@gmail.com>
2023-01-09 11:10:07 -0800

    Project initialization

---
+++ src/main/java/com/codingart/mycompta/MyComptaApplication.java
@@ -0,0 +1,13 @@
+package com.codingart.mycompta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyComptaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyComptaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codingart/mycompta/MyComptaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingart.mycompta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyComptaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
